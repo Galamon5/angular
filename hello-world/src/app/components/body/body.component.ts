36f63e419ebf5e3d00ea2a29c07237b56d09c189
Galamon5 <galamon5@hotmail.com>
2018-06-05 23:29:54 -0500

    Hello World!

--- hello-world/src/app/components/body/body.component.ts
+++ hello-world/src/app/components/body/body.component.ts
@@ -4,5 +4,10 @@ import { Component } from '@angular/core';
   templateUrl: './body.component.html'
 })
 export class BodyComponent{
-
+  mostrar=true;
+  frase:any = {
+    mensaje: 'im Batman',
+    autor: 'batman'
+  };
+  personajes:string[] = ['batman','superman','joker' ];
 }
